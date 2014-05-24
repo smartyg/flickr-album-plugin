26aa1daeceb361658c5cd170f592338398c84d4f
Martijn Goedhart <goedhart.martijn@gmail.com>
2014-05-24 13:13:35 +0200

    Initial commit

---
+++ flickr-album-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/** kate: indent-mode cstyle;
+ * Plugin Name: Flickr Album Plugin
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: A plugin to display one, multiple or all flickr albums
+ * Version: 0.1
+ * Author: Martijn Goedhart
+ * Author URI: http://URI_Of_The_Plugin_Author
+ * License: GPLv2+
+ * Text Domain: fab
+ * Domain Path: /l10n/
+ */
+
+?>
